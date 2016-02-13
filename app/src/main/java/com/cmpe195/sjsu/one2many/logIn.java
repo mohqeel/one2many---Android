3980058fd6b159deb51ca11c25599b34f11a171a
Mohammed Gaggutur <Aqeel@Mohammeds-MacBook-Pro.local>
2016-02-12 17:39:31 -0800

    added login page and cleaned up sign up logic

---
+++ app/src/main/java/com/cmpe195/sjsu/one2many/logIn.java
@@ -0,0 +1,13 @@
+package com.cmpe195.sjsu.one2many;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class logIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
